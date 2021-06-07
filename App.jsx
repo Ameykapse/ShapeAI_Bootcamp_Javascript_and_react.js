61d9857fe71dcdb84dd48e0b96b64154306fb83c
Ameykapse <84178323+Ameykapse@users.noreply.github.com>
2021-06-07 11:12:44 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import App from "./Header";
+import App from "./Footer";
+import App from "./Note";
+
+function App()
+{
+	return {
+		<div>
+			<Header />
+			<Footer />
+			<Note />
+			<Note />
+			<Note />
+			<Note />
+		</div>
+	};
+}
+
+export default App;
---
+++ Note.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function note()
+{
+	return {
+		<div className = "note">
+			<h1>javascript and react.js</h1>
+			<p>
+				The bootcamp was amazing.It was taken by shaurya sinha sir.We learned everything from scratch.
+			</p>
+		</div>
+	};
+}
+
+export default Note;
